5e90c5c60de702bc6b25ed6336761fb0ba762095
shohei.imamura <shoboc075@gmail.com>
2018-06-19 03:33:29 +0000

    follow

--- app/User.php
+++ app/User.php
@@ -18,11 +18,12 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
 
-    /**
+     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
+     
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -31,5 +32,59 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
-}
+    
+    
+    /*belongsToMany()は
+    
+    */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
+    }
+    
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+
+    
+    
+    //followするときの動作関数
+    public function follow($userId)
+    {
+        $exist = $this->is_following($userId);  //既にfollowしてないか確認
+        $its_me = $this->id == $userId;  //自分じゃないか確認
+        
+        if ($exist || $its_me) {
+            return false;  //followを既にしていたらfalseを返す
+            
+        }else {
+            $this->followings()->attach($userId);
+            return true;  //followしていなかったらfollowする
+        }
+    }
+    
+    public function unfollow($userId)
+    {
+    // confirming if already following
+         $exist = $this->is_following($userId);
+    // confirming that it is not you
+         $its_me = $this->id == $userId;
+
+
+    if ($exist && !$its_me) {
+        // stop following if following
+        $this->followings()->detach($userId);
+        return true;
+    } else {
+        // do nothing if not following
+        return false;
+    }
+    }
+
+
+    public function is_following($userId) {
+    return $this->followings()->where('follow_id', $userId)->exists();
+    }
+    }
 
